d0a5cc7bae949ceda92c7574517f23bea8635246
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-08-27 22:27:42 +0200

    using signals in notion-database-item

--- src/app/modules/notion/components/notion-database-item/notion-database-item.component.ts
+++ src/app/modules/notion/components/notion-database-item/notion-database-item.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, input, Input, output, Output } from '@angular/core';
 import { NotionDatabaseItem } from "@modules/notion/services/notion.service";
 import { NotionBadgeStatusComponent } from '../notion-badge-status/notion-badge-status.component';
 import { NotionItemTagsComponent } from '../notion-item-tags/notion-item-tags.component';
@@ -9,12 +9,17 @@ import { NotionItemTagsComponent } from '../notion-item-tags/notion-item-tags.co
   standalone: true,
   imports: [NgClass, NotionBadgeStatusComponent, NotionItemTagsComponent],
   templateUrl: './notion-database-item.component.html',
-  styleUrl: './notion-database-item.component.scss'
+  styleUrl: './notion-database-item.component.scss',
+  host: {
+    '(click)': 'itemSelected.emit(item)',
+    class: 'dark:hover:bg-neutral-600/50 hover:bg-neutral-100/50 px-2 py-1 rounded-md flex justify-between cursor-pointer items-baseline'
+  }
 })
 export class NotionDatabaseItemComponent {
 
-  @Input() item!: NotionDatabaseItem
-  @Output() itemSelected = new EventEmitter<NotionDatabaseItem>()
+
+  public item = input.required<NotionDatabaseItem>()
+  public itemSelected = output<NotionDatabaseItem>()
 
 
 }
