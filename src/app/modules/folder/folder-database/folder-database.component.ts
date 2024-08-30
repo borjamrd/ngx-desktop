314ce40c636be6fad7a91c6f4b75b35eadd299eb
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-08-30 13:06:02 +0200

    make folder database container reponsive

--- src/app/modules/folder/folder-database/folder-database.component.ts
+++ src/app/modules/folder/folder-database/folder-database.component.ts
@@ -1,19 +1,30 @@
 import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
-import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { SystemElement } from 'app/shared/types/system-element.type';
 import { CellContainerComponent } from '../../../shared/components/cell-container/cell-container.component';
 
 @Component({
   selector: 'bm-folder-database',
   standalone: true,
-  imports: [CellContainerComponent, NgFor, DragDropModule],
+  imports: [CellContainerComponent, NgFor, DragDropModule, NgClass],
+
   templateUrl: './folder-database.component.html',
-  styleUrl: './folder-database.component.scss'
+  styleUrl: './folder-database.component.scss',
+  host: {
+    class: 'block px-2'
+  }
 })
 export class FolderDatabaseComponent {
+
   @Input() layout: SystemElement[] = []
+
+  @ViewChild('elementList') elementList!: ElementRef;
+
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.layout, event.previousIndex, event.currentIndex);
   }
+
+
+
 }
