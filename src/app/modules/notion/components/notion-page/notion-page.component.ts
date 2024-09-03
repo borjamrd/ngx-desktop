260f4ce80c459f8a2935da8cf0e4ddc064d3583e
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-09-03 18:30:38 +0200

    markforcheck changes applied on notion page

--- src/app/modules/notion/components/notion-page/notion-page.component.ts
+++ src/app/modules/notion/components/notion-page/notion-page.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule, NgTemplateOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnChanges, signal, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, effect, inject, input, OnChanges, signal, SimpleChanges } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NotionService } from '@modules/notion/services/notion.service';
+import { finalize } from 'rxjs';
 import { NotionBlock, NotionDatabaseItem } from '../../types/notion.interface';
+import { NotionBadgeStatusComponent } from '../notion-badge-status/notion-badge-status.component';
 import { NotionBlockComponent } from '../notion-block/notion-block.component';
+import { NotionItemTagsComponent } from "../notion-item-tags/notion-item-tags.component";
 import { NotionPageHeaderComponent } from '../notion-page-header/notion-page-header.component';
 
 @Component({
@@ -51,6 +54,7 @@ export class NotionPageComponent implements OnChanges {
           const pageBlock = this.notionBlocks.find((block) => block.type === 'page') as NotionBlock;
           this.iconPage.set(pageBlock.format?.page_icon);
         }
+        this.cdr.markForCheck();
       });
   }
 
