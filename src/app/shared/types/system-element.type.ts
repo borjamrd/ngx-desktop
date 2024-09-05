eb7257d8e2fbd809fc0509813302e22df74f73c1
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-09-05 12:41:44 +0200

    option for git files added

--- src/app/shared/types/system-element.type.ts
+++ src/app/shared/types/system-element.type.ts
@@ -11,8 +11,7 @@ export enum ElementType {
     SYSTEM_FOLDER = 'system-folder'
 }
 
-export interface FolderElement { id: string, name: string }
-
+// export interface FolderElement { id: string, name: string }
 
 export interface SystemElement extends KtdGridLayoutItem {
     icon: string;
@@ -42,237 +41,256 @@ export const defaultLayout: SystemElement[] = [
         y: 0,
         w: 1,
         h: 1,
-        children: [{
-            icon: 'folder',
-            name: 'My tech',
-            type: ElementType.FOLDER,
-            id: crypto.randomUUID(),
-            x: 0,
-            y: 0,
-            w: 1,
-            h: 1,
-            hasChildren: true,
-            resizable: false,
-            children: [
-                {
-                    icon: 'folder',
-                    name: 'Backend',
-                    type: ElementType.FOLDER,
-                    id: crypto.randomUUID(),
-                    x: 0,
-                    y: 0,
-                    w: 1,
-                    h: 1,
-                    hasChildren: true,
-                    children: [
-                        {
-                            icon: 'folder',
-                            name: 'Folder2',
-                            type: ElementType.FOLDER,
-                            id: crypto.randomUUID(),
-                            x: 0,
-                            y: 0,
-                            w: 1,
-                            h: 1,
-                            hasChildren: false,
-                            children: []
-                        },
-                        {
-                            icon: 'folder',
-                            name: 'Folder3',
-                            type: ElementType.FOLDER,
-                            id: crypto.randomUUID(),
-                            x: 1,
-                            y: 1,
-                            w: 1,
-                            h: 1,
-                            hasChildren: false,
-                            children: []
-                        }
-                    ]
-                },
-                {
-                    icon: 'folder',
-                    name: 'Frontend',
-                    type: ElementType.FOLDER,
-                    id: crypto.randomUUID(),
-                    x: 1,
-                    y: 1,
-                    w: 1,
-                    h: 1,
-                    hasChildren: false,
-                    children: []
-                },
-                {
-                    icon: 'folder',
-                    name: 'Common',
-                    type: ElementType.FOLDER,
-                    id: crypto.randomUUID(),
-                    x: 1,
-                    y: 1,
-                    w: 1,
-                    h: 1,
-                    hasChildren: false,
-                    children: [
-                        {
-                            icon: 'javascript',
-                            name: 'Folder2',
-                            type: ElementType.FILE,
-                            id: crypto.randomUUID(),
-                            x: 0,
-                            y: 0,
-                            w: 1,
-                            h: 1,
-                            fileData: {
-                                url: 'assets/icons/javascript.svg.svg',
-                                fileType: 'image/svg+xml',
+        children: [
+            {
+                icon: 'folder',
+                name: 'My tech',
+                type: ElementType.FOLDER,
+                id: crypto.randomUUID(),
+                x: 0,
+                y: 0,
+                w: 1,
+                h: 1,
+                hasChildren: true,
+                resizable: false,
+                children: [
+                    {
+                        icon: 'folder',
+                        name: 'Backend',
+                        type: ElementType.FOLDER,
+                        id: crypto.randomUUID(),
+                        x: 0,
+                        y: 0,
+                        w: 1,
+                        h: 1,
+                        hasChildren: true,
+                        children: [
+                            {
+                                icon: 'folder',
+                                name: 'Folder2',
+                                type: ElementType.FOLDER,
+                                id: crypto.randomUUID(),
+                                x: 0,
+                                y: 0,
+                                w: 1,
+                                h: 1,
+                                hasChildren: false,
+                                children: []
                             },
-                            hasChildren: false,
-                            children: []
-                        },
-                        {
-                            icon: 'typescript',
-                            name: 'Typescript',
-                            type: ElementType.FILE,
-                            id: crypto.randomUUID(),
-                            fileData: {
-                                url: 'assets/icons/typescript.svg',
-                                fileType: 'image/svg+xml',
+                            {
+                                icon: 'folder',
+                                name: 'Folder3',
+                                type: ElementType.FOLDER,
+                                id: crypto.randomUUID(),
+                                x: 1,
+                                y: 1,
+                                w: 1,
+                                h: 1,
+                                hasChildren: false,
+                                children: []
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'folder',
+                        name: 'Frontend',
+                        type: ElementType.FOLDER,
+                        id: crypto.randomUUID(),
+                        x: 1,
+                        y: 1,
+                        w: 1,
+                        h: 1,
+                        hasChildren: false,
+                        children: []
+                    },
+                    {
+                        icon: 'folder',
+                        name: 'Common',
+                        type: ElementType.FOLDER,
+                        id: crypto.randomUUID(),
+                        x: 1,
+                        y: 1,
+                        w: 1,
+                        h: 1,
+                        hasChildren: false,
+                        children: [
+                            {
+                                icon: 'javascript',
+                                name: 'Folder2',
+                                type: ElementType.FILE,
+                                id: crypto.randomUUID(),
+                                x: 0,
+                                y: 0,
+                                w: 1,
+                                h: 1,
+                                fileData: {
+                                    url: 'assets/icons/javascript.svg.svg',
+                                    fileType: 'image/svg+xml',
+                                },
+                                hasChildren: false,
+                                children: []
                             },
-                            x: 1,
-                            y: 1,
-                            w: 1,
-                            h: 1,
+                            {
+                                icon: 'typescript',
+                                name: 'Typescript',
+                                type: ElementType.FILE,
+                                id: crypto.randomUUID(),
+                                fileData: {
+                                    url: 'assets/icons/typescript.svg',
+                                    fileType: 'image/svg+xml',
+                                },
+                                x: 1,
+                                y: 1,
+                                w: 1,
+                                h: 1,
 
-                        },
-                        {
-                            icon: 'angular',
-                            name: 'angular',
-                            type: ElementType.FILE,
-                            id: crypto.randomUUID(),
-                            fileData: {
-                                url: 'assets/icons/angular.svg',
-                                fileType: 'image/svg+xml',
                             },
-                            x: 1,
-                            y: 1,
-                            w: 1,
-                            h: 1,
+                            {
+                                icon: 'angular',
+                                name: 'angular',
+                                type: ElementType.FILE,
+                                id: crypto.randomUUID(),
+                                fileData: {
+                                    url: 'assets/icons/angular.svg',
+                                    fileType: 'image/svg+xml',
+                                },
+                                x: 1,
+                                y: 1,
+                                w: 1,
+                                h: 1,
 
-                        }
-                    ]
+                            }
+                        ]
+                    },
+
+
+                ],
+            },
+            {
+                'icon': 'folder',
+                name: 'Spotify',
+                type: ElementType.DESKTOP_WIDGET,
+                id: crypto.randomUUID(),
+                x: 12,
+                y: 0,
+                w: 4,
+                h: 2,
+                customClass: 'bg-slate-500/40',
+                component: SpotifyWidgetComponent,
+                resizable: true
+            },
+            {
+                icon: 'notion',
+                name: 'Notion',
+                id: crypto.randomUUID(),
+                x: 1,
+                y: 0,
+                w: 1,
+                h: 1,
+                hasChildren: false,
+                type: ElementType.APPLICATION,
+                resizable: false
+            },
+            {
+                icon: 'medium',
+                name: 'Medium',
+                id: crypto.randomUUID(),
+                x: 2,
+                y: 0,
+                w: 1,
+                h: 1,
+                hasChildren: false,
+                type: ElementType.APPLICATION,
+                resizable: false
+            },
+            {
+                icon: 'gmail',
+                name: 'Gmail',
+                id: crypto.randomUUID(),
+                x: 0,
+                y: 1,
+                w: 1,
+                h: 1,
+                hasChildren: false,
+                type: ElementType.APPLICATION,
+                resizable: false
+            },
+            {
+                icon: 'trash',
+                name: 'Papelera de reciclaje',
+                id: crypto.randomUUID(),
+                x: 0,
+                y: 2,
+                w: 1,
+                h: 1,
+                hasChildren: true,
+                children: [
+                    {
+                        icon: 'gif',
+                        name: 'ahahah.gif',
+                        id: crypto.randomUUID(),
+                        x: 0,
+                        y: 3,
+                        w: 1,
+                        h: 1,
+                        hasChildren: false,
+                        type: ElementType.FILE,
+                        fileData: {
+                            url: 'assets/images/ahahah.gif',
+                            fileType: 'image/gif',
+                        },
+                    },
+                ],
+                type: ElementType.FOLDER,
+                resizable: false,
+            },
+            {
+                icon: 'pdf',
+                name: 'CV_Borja_ES.pdf',
+                id: crypto.randomUUID(),
+                x: 0,
+                y: 3,
+                w: 1,
+                h: 1,
+                hasChildren: false,
+                type: ElementType.FILE,
+                fileData: {
+                    url: 'assets/files/ES_BorjaMunoz_FullStack_CV.pdf',
+                    fileType: 'application/pdf',
                 },
-
-
-            ],
-        },
-        {
-            'icon': 'folder',
-            name: 'Spotify',
-            type: ElementType.DESKTOP_WIDGET,
-            id: crypto.randomUUID(),
-            x: 12,
-            y: 0,
-            w: 4,
-            h: 2,
-            customClass: 'bg-slate-500/40',
-            component: SpotifyWidgetComponent,
-            resizable: true
-        }, {
-            icon: 'notion',
-            name: 'Notion',
-            id: crypto.randomUUID(),
-            x: 1,
-            y: 0,
-            w: 1,
-            h: 1,
-            hasChildren: false,
-            type: ElementType.APPLICATION,
-            resizable: false
-        },
-        {
-            icon: 'medium',
-            name: 'Medium',
-            id: crypto.randomUUID(),
-            x: 2,
-            y: 0,
-            w: 1,
-            h: 1,
-            hasChildren: false,
-            type: ElementType.APPLICATION,
-            resizable: false
-        },
-        {
-            icon: 'gmail',
-            name: 'Gmail',
-            id: crypto.randomUUID(),
-            x: 0,
-            y: 1,
-            w: 1,
-            h: 1,
-            hasChildren: false,
-            type: ElementType.APPLICATION,
-            resizable: false
-        },
-        {
-            icon: 'trash',
-            name: 'Papelera de reciclaje',
-            id: crypto.randomUUID(),
-            x: 0,
-            y: 2,
-            w: 1,
-            h: 1,
-            hasChildren: false,
-            type: ElementType.FOLDER,
-            resizable: false
-        },
-        {
-            icon: 'pdf',
-            name: 'CV_Borja_ES.pdf',
-            id: crypto.randomUUID(),
-            x: 0,
-            y: 3,
-            w: 1,
-            h: 1,
-            hasChildren: false,
-            type: ElementType.FILE,
-            fileData: {
-                url: 'assets/files/ES_BorjaMunoz_FullStack_CV.pdf',
-                fileType: 'application/pdf',
             },
-        },
-        {
-            icon: 'pdf',
-            name: 'CV_Borja_EN.pdf',
-            id: crypto.randomUUID(),
-            x: 0,
-            y: 4,
-            w: 1,
-            h: 1,
-            hasChildren: false,
-            type: ElementType.FILE,
+            {
+                icon: 'pdf',
+                name: 'CV_Borja_EN.pdf',
+                id: crypto.randomUUID(),
+                x: 0,
+                y: 4,
+                w: 1,
+                h: 1,
+                hasChildren: false,
+                type: ElementType.FILE,
 
-            fileData: {
-                url: 'assets/files/EN_BorjaMunoz_FullStack_CV_English.pdf',
-                fileType: 'application/pdf',
+                fileData: {
+                    url: 'assets/files/EN_BorjaMunoz_FullStack_CV_English.pdf',
+                    fileType: 'application/pdf',
+                },
             },
-        },
-        {
-            icon: 'markdown',
-            name: 'readme',
-            id: crypto.randomUUID(),
-            x: 0,
-            y: 5,
-            w: 1,
-            h: 1,
-            hasChildren: false,
-            type: ElementType.FILE,
+            {
+                icon: 'markdown',
+                name: 'readme',
+                id: crypto.randomUUID(),
+                x: 0,
+                y: 5,
+                w: 1,
+                h: 1,
+                hasChildren: false,
+                type: ElementType.FILE,
 
-            fileData: {
-                url: 'assets/files/readme.md',
-                fileType: 'text/markdown',
+                fileData: {
+                    url: 'assets/files/readme.md',
+                    fileType: 'text/markdown',
+                },
             },
-        },
         ]
     }
 ]
