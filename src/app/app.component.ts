600ea0ca8b00d43acd43d0c58e0f801f1d3c87dc
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-09-16 19:59:33 +0200

    check metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,11 +33,11 @@ export class AppComponent {
   }
 
   ngOnInit(): void {
-    this.meta.updateTag({ name: 'description', content: 'Ngx desktop is an Angular desktop client' });
-    this.meta.updateTag({ name: 'title', content: 'Ngx-desktop - Angular' });
-    this.meta.updateTag({ name: 'keywords', content: 'angular, defer, typescript, desktop-rocket' });
-    this.meta.updateTag({ name: 'author', content: 'Borja Muñoz' });
-    this.meta.updateTag({ property: 'og:image', content: 'assets/images/artic_monkeys.jpg' });
+    this.meta.addTag({ name: 'description', content: 'Ngx desktop is an Angular desktop client' });
+    this.meta.addTag({ name: 'title', content: 'Ngx-desktop - Angular' });
+    this.meta.addTag({ name: 'keywords', content: 'angular, defer, typescript, desktop-rocket' });
+    this.meta.addTag({ name: 'author', content: 'Borja Muñoz' });
+    this.meta.addTag({ property: 'og:image', content: 'assets/images/artic_monkeys.jpg' });
     // this.meta.addTag({ name: 'twitter:image', content: 'https://your-domain.com/assets/preview-image.jpg' });
   }
 }
