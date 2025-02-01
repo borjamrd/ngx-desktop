b199c769d54d5ff9e1ce4f96f801d1a390d34d9c
Borja <borjamrd1@gmail.com>
2025-02-01 12:55:10 +0100

    feat: upgrade from v17 to v18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,17 @@
-import { ApplicationConfig, importProvidersFrom, ɵprovideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import {} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { routes } from './app.routes';
-import { provideHighlightOptions } from 'ngx-highlightjs';
 import { provideAngularQuery, QueryClient } from '@tanstack/angular-query-experimental';
+import { provideHighlightOptions } from 'ngx-highlightjs';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     provideAnimationsAsync(),
-    importProvidersFrom(HttpClientModule),
     provideAngularQuery(new QueryClient()),
     provideHighlightOptions({
       fullLibraryLoader: () => import('highlight.js'),
